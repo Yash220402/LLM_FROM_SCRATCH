38b71a087db0f188403b0e43e67852c65afd5047
Yash220402 <yash220402@gmail.com>
2026-07-12 21:10:26 +0530

    sample with larger batch size

--- dataloader.py
+++ dataloader.py
@@ -50,11 +50,12 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 
 dataloader = create_dataloader_v1(raw_text, 
-                                  batch_size=1,
+                                  batch_size=8,
                                   max_length=4,
-                                  stride=1,
+                                  stride=4,
                                   shuffle=False)
 
 data_iter = iter(dataloader)
-first_batch = next(data_iter)
-print(first_batch)
+inputs, targets = next(data_iter)
+print("Inputs: \n", inputs)
+print("Targets: \n", targets)
