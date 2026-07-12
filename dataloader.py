cb8fa31da959a7486a6d0485a6cb81561c0be05e
Yash220402 <yash220402@gmail.com>
2026-07-12 21:47:27 +0530

    create token embeddings

--- dataloader.py
+++ dataloader.py
@@ -66,3 +66,14 @@ data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 print("Token IDs: \n", inputs)
 print("Input Shape: \n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print(token_embeddings.shape)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print(pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print(input_embeddings.shape)
