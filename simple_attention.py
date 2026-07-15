db7690728cf8e955d88ee2cdfb68b0eab354f637
Yash220402 <yash220402@gmail.com>
2026-07-15 17:06:33 +0530

    self-attention using pytorch linear layer

--- simple_attention.py
+++ simple_attention.py
@@ -35,3 +35,26 @@ class SelfAttention_v1(nn.Module):
 torch.manual_seed(123)
 sa_v1 = SelfAttention_v1(d_in, d_out)
 print(sa_v1(inputs))
+
+# self-attention using pytorch's linear layers
+class SelfAttention_v2(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+    
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print(sa_v2(inputs))
