d75fc52119e1c92e30e2e02123541a1a6fdc4768
Yash220402 <yash220402@gmail.com>
2026-07-19 23:53:46 +0530

    mha implementation

--- MultiHeadAttention.py
+++ MultiHeadAttention.py
@@ -48,3 +48,22 @@ class MultiHeadAttention(nn.Module):
         )
         context_vec = self.out_proj(context_vec)
         return context_vec
+    
+
+a = torch.tensor([[[[0.2745, 0.6584, 0.2775, 0.8573],
+                    [0.8993, 0.0390, 0.9268, 0.7388],
+                    [0.7179, 0.7058, 0.9156, 0.4340]],
+
+                    [[0.0772, 0.3565, 0.1479, 0.5331],
+                    [0.4066, 0.2318, 0.4545, 0.9737],
+                    [0.4606, 0.5159, 0.4220, 0.5786]]]])
+
+print(a @ a.transpose(2, 3))
+
+first_head = a[0, 0, :, :]
+first_res = first_head @ first_head.T 
+print("First head:\n", first_res)
+
+second_head = a[0, 0, :, :]
+second_res = second_head @ second_head.T 
+print("Second head:\n", second_res)
