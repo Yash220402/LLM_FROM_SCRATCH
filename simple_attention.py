d95a38fe4ceb61dc3277fa1131cabd1f792c5d31
Yash220402 <yash220402@gmail.com>
2026-07-19 23:27:40 +0530

    implement multihead attention

--- simple_attention.py
+++ simple_attention.py
@@ -112,3 +112,16 @@ context_length = batch.shape[1]
 ca = CausalAttention(d_in, d_out, context_length, dropout=0.0)
 context_vecs = ca(batch)
 print("context_vecs.shape:", context_vecs.shape)
+
+
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(d_in, d_out, context_length, dropout, qkv_bias)
+             for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
