ed5a93db8dabf78f37b55800e222a7113e0548cb
Yash220402 <yash220402@gmail.com>
2026-07-24 23:09:45 +0530

    example deep nn

--- llm.py
+++ llm.py
@@ -1,11 +1,11 @@
 GPT_CONFIG_124M = {
-    "vocab_size": 50257, # Vocabulary size
+    "vocab_size": 50257,    # Vocabulary size
     "context_length": 1024, # Context length
-    "emb_dim": 768, # Embedding dimension
-    "n_heads": 12, # Number of attention heads
-    "n_layers": 12, # Number of layers
-    "drop_rate": 0.1, # Dropout rate
-    "qkv_bias": False # Query-Key-Value bias
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
 }
 
 import torch
@@ -119,6 +119,52 @@ for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
     plt.title(f"{label} activation function")
     plt.xlabel("x")
     plt.ylabel(f"{label} (x)")
-    plt.grid(true)
+    plt.grid(True)
 plt.tight_layout()
 plt.show()
+
+
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(out.shape)
+
+
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]),
+            GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]),
+            GELU())
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+            return x
