825ddbd2c78a7656de5cd0c93ca6e30f6ca5d73b
Yash220402 <yash220402@gmail.com>
2026-07-26 16:03:29 +0530

    text gpt model output

--- gpt_model.py
+++ gpt_model.py
@@ -96,6 +96,21 @@ class GPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
 
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_cond)
+
+        logits = logits[:, -1, :]
+
+        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+
+        idx = torch.cat((idx, idx_next), dim=1)
+
+    return idx
+
 
 def main():
     GPT_CONFIG_124M = {
@@ -114,7 +129,7 @@ def main():
 
     start_context = "Hello, I am"
 
-    tokenizer = tiktoken.get_encoding("gpt_2")
+    tokenizer = tiktoken.get_encoding("gpt2")
     encoded = tokenizer.encode(start_context)
     encoded_tensor = torch.tensor(encoded).unsqueeze(0)
 
@@ -123,6 +138,18 @@ def main():
     print("Encoded input text:", encoded)
     print("encoded_tensor.shape:", encoded_tensor.shape)
 
+    out = generate_text_simple(
+        model=model,
+        idx=encoded_tensor,
+        max_new_tokens=10,
+        context_size=GPT_CONFIG_124M["context_length"]
+    )
+    decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+
+    print(f"\n\n{50*'='}\n{22*' '}OUT\n{50*'='}")
+    print("\nOutput:", out)
+    print("Output length:", len(out[0]))
+    print("Output text:", decoded_text)
 
 
 if __name__ == "__main__":
