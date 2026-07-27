62b6bf53478642565a552fd26453e2f00947e75e
Yash220402 <yash220402@gmail.com>
2026-07-27 20:49:05 +0530

    compute cross entropy loss

--- gpt_train.py
+++ gpt_train.py
@@ -53,3 +53,25 @@ print("Token IDs:\n", token_ids)
 print(f"Targets batch 1: {token_ids_to_text(targets[0], tokenizer)}")
 print(f"Outputs batch 1:"
       f" {token_ids_to_text(token_ids[0].flatten(), tokenizer)}")
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0,1,2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0,1,2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
+
+logits_flat = logits.flatten(0, 1)
+targets_flat = targets.flatten()
+print("Flattened Logits:", logits_flat.shape)
+print("Flattened targets:", targets_flat.shape)
+
+loss = torch.nn.functional.cross_entropy(logits_flat, targets_flat)
+print(loss)
