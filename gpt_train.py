90c706bb89413d43e5d0946ff8633fa66fc464a9
Yash220402 <yash220402@gmail.com>
2026-07-29 18:53:25 +0530

    set up dataloaders

--- gpt_train.py
+++ gpt_train.py
@@ -1,3 +1,6 @@
+import os
+import requests
+
 import torch
 import tiktoken
 from gpt_model import GPTModel, generate_text_simple
@@ -48,36 +51,61 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
 
     return total_loss / num_batches
 
-train_ratio = 0.90
-split_idx = int(train_ratio * len(text_data))
-train_data = text_data[:split_idx]
-val_data = text_data[split_idx:]
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
 
-torch.manual_seed(123)
+    model.train()
+    return train_loss, val_loss
 
-train_loader = create_dataloader_v1(
-    train_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=True,
-    shuffle=True,
-    num_workers=0
-)
-val_loader = create_dataloader_v1(
-    val_data,
-    batch_size=2,
-    max_length=GPT_CONFIG_124M["context_length"],
-    stride=GPT_CONFIG_124M["context_length"],
-    drop_last=False,
-    shuffle=False,
-    num_workers=0
-)
 
-print("Train loader:")
-for x, y in train_loader:
-    print(x.shape, y.shape)
 
-print("\nValidation loader:")
-for x, y in val_loader:
-    print(x.shape, y.shape)
+def main(gpt_config, settings):
+    torch.manual_seed(123)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    file_path = "the-verdict.txt"
+    url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
+
+    if not os.path.exists(file_path):
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+        text_data = response.text
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+    else:
+        with open(file_path, "r", encoding="utf-8") as file:
+            file.read(text_data)
+
+    # initialize model 
+    model = GPTModel(gpt_config)
+    model.to(device)
+    optimzer = torch.optim.AdamW(
+        model.parameters(), lr=settings['learning_rate'], weight_decay=settings["weight_decay"]
+    )
+
+    # set up dataloaders
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+
+    train_loader = create_dataloader_v1(
+        text_data[:split_idx],
+        batch_size=settings["batch_size"],
+        max_length=gpt_config["context_length"],
+        stride=gpt_config["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+    val_loader = create_dataloader_v1(
+        text_data[split_idx:],
+        batch_size=settings["batch_size"],
+        max_length=gpt_config["context_length"],
+        stride=gpt_config["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
