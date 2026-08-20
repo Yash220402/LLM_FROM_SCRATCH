9a26db741a7199851b7323664be52cfbd6de7537
Yash220402 <yash220402@gmail.com>
2026-08-20 23:05:45 +0530

    load weights

--- gpt_generate.py
+++ gpt_generate.py
@@ -110,4 +110,11 @@ def load_gpt2_params_from_tf_ckpt(ckpt_path, settings):
 def assign(left, right):
     if left.shape != right.shape:
         raise ValueError(f"Shape mismatch. Left: {left.shape}, Right: {right.shape}")
-    return torch.nn.Parameter(torch.tensor(right))  
+    return torch.nn.Parameter(torch.tensor(right))
+
+
+def load_weights_into_gpt(gpt, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params["wpe"])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params["wte"])
+
+    
